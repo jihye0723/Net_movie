c7c0fdc149d0b16d0612f9d53649891a0589ad6a
jihye0723 <88600739+jihye0723@users.noreply.github.com>
2023-08-15 16:33:50 +0900

    Create: Project

---
+++ NetMovie/src/main/java/com/example/netmovie/NetMovieApplication.java
@@ -0,0 +1,13 @@
+package com.example.netmovie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetMovieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NetMovieApplication.class, args);
+	}
+
+}
---
+++ NetMovie/src/test/java/com/example/netmovie/NetMovieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.netmovie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetMovieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
